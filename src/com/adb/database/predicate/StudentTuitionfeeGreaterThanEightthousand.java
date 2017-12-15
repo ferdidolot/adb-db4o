bfa0588021911376d2b93ede0f0251ceec65e55c
Ferdiansyah Dolot <ferdi.dolot@gmail.com>
2017-12-15 17:08:38 +0100

    More more refactor, add predicate

---
+++ src/com/adb/database/predicate/StudentTuitionfeeGreaterThanEightthousand.java
@@ -0,0 +1,10 @@
+package com.adb.database.predicate;
+
+import com.adb.model.Student;
+import com.db4o.query.Predicate;
+
+public class StudentTuitionfeeGreaterThanEightthousand extends Predicate<Student>{
+    public boolean match(Student student){
+        return student.getYearlyTuitionFee() > 8000;
+    }
+}
