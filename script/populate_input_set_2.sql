4ff9a8c090c90acd00ea328603f60bdd94f5121f
Ferdiansyah Dolot <ferdi.dolot@gmail.com>
2017-12-18 15:58:21 +0100

    Additional sql script

---
+++ script/populate_input_set_2.sql
@@ -0,0 +1,4 @@
+copy student FROM 'D:\Dropbox\bdma\ulb\adb-db4o\input\student10000.in' DELIMITER ',' CSV;
+copy professor FROM 'D:\Dropbox\bdma\ulb\adb-db4o\input\professor100.in' DELIMITER ',' CSV;
+copy course FROM 'D:\Dropbox\bdma\ulb\adb-db4o\input\course100.in' DELIMITER ',' CSV;
+copy coursetaken FROM 'D:\Dropbox\bdma\ulb\adb-db4o\input\coursetaken_10000_100.in' DELIMITER ',' CSV;
---
+++ script/truncate_table.sql
@@ -0,0 +1,4 @@
+truncate table student cascade;
+truncate table professor cascade;
+truncate table course cascade;
+truncate table coursetaken cascade;
