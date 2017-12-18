fa383ed6fd4cb488f7f0b5890f10e7392dc94371
Ferdiansyah Dolot <ferdi.dolot@gmail.com>
2017-12-18 20:20:28 +0100

    Refactor

--- src/com/adb/database/predicate/StudentTuitionFeePredicate.java
+++ src/com/adb/database/predicate/StudentTuitionFeePredicate.java
@@ -3,7 +3,7 @@ package com.adb.database.predicate;
 import com.adb.model.Student;
 import com.db4o.query.Predicate;
 
-public class StudentTuitionFee extends Predicate<Student>{
+public class StudentTuitionFeePredicate extends Predicate<Student>{
     public boolean match(Student student){
         return student.getYearlyTuitionFee() > 8000;
     }
